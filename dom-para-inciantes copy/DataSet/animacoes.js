f943811df9ac21831fa38b3891e9000f55d73a29
Arrudxx <d4niel.arruda@gmail.com>
2022-10-06 22:18:50 -0300

    Aula sobre Dataset Finalizada

---
+++ dom-para-inciantes copy/DataSet/animacoes.js
@@ -0,0 +1,96 @@
+function initTabNav() {
+  const tabMenu = document.querySelectorAll("[data-tab='menu'] li");
+  const tabContent = document.querySelectorAll("[data-tab='content'] section");
+
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      const direcao = tabContent[index].dataset.anime;
+      tabContent[index].classList.add("ativo", direcao);
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(
+    "[data-anime='accordion'] dt"
+  );
+  const activeClasse = "ativo";
+
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClasse);
+    accordionList[0].nextElementSibling.classList.add(activeClasse);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClasse);
+      this.nextElementSibling.classList.toggle(activeClasse);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll(
+    '[data-menu="suave"] a[href^="#"]'
+  );
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = this.getAttribute("href");
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    //FORMA ALTERNATIVA
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: "smooth",
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initScrollSuave();
+
+function intAnimacaoScroll(params) {
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  const windowMetade = window.innerHeight * 0.6;
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+intAnimacaoScroll();
---
+++ dom-para-inciantes copy/DataSet/script-aula.js
@@ -0,0 +1,10 @@
+// const div = document.querySelector("[data-cor]");
+// console.log(Object.prototype.toString.call(div));
+
+// div.dataset.height = 1000;
+
+// delete div.dataset.width;
+
+// div.dataset.totalHeight = 2000;
+
+// console.log(div.dataset);
