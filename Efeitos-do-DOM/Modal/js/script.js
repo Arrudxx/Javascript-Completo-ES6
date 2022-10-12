e382ad83b192c7900045b0c1e4851ad08ea73dcc
Arrudxx <d4niel.arruda@gmail.com>
2022-10-11 21:47:04 -0300

    Aula Sobre Modal Finalizada

--- Efeitos-do-DOM/Modal/js/script.js
+++ Efeitos-do-DOM/Modal/js/script.js
@@ -1,9 +1,11 @@
 import initScrollSuave from "./modules/scroll-suave.js";
 import intAnimacaoScroll from "./modules/scroll-animacao.js";
-import initAccordion from "./modules/Accordion.js";
-import initTabNav from "./modules/Tabnav.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
 
 initScrollSuave();
 intAnimacaoScroll();
 initAccordion();
 initTabNav();
+initModal();
