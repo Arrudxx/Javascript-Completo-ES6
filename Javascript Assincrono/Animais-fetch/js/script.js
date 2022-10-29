036bb580f20bcf8bffba5554982de29a0060c88c
Arrudxx <d4niel.arruda@gmail.com>
2022-10-28 22:44:22 -0300

    aula de fetch no projeto de animais finalizada

---
+++ Javascript Assincrono/Animais-fetch/js/script.js
@@ -0,0 +1,23 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import intAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+
+import initDateObject from "./modules/date-object.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
+
+initScrollSuave();
+intAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+
+initDateObject();
+initFetchAnimais();
